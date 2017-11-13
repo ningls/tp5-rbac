beba2ce5c6687eea6188b3fa97e4132cf7a4e45e
hiRainn <wr_sorarara@yahoo.co.jp>
2017-11-14 00:33:17 +0800

    create_admin

--- app/behind/controller/Base.php
+++ app/behind/controller/Base.php
@@ -1,8 +1,73 @@
 <?php
 namespace app\behind\controller;
 use \app\common\controller\Cbase;
+use \think\Db;
 
 class Base extends Cbase
 {
+	protected $error_msg;
+	protected $admin_data = []; 
+	/**
+	* 检测是否已经含有表以便生成admin
+	*/
+	public function _initialize()
+	{
+		$action = request()->action();
+		if(in_array($action,['create_auth', 'auth_exists', 'create_admin', 'error_page'])) return false;
+		try{
+			Db::Query("show tables");
+		}
+		catch(\PDOException $e){
+			exit('数据库配置错误');
+		}
+		catch(\think\Exception $e) {
+			exit('未配置数据库');
+		}
+		if(!$this->auth_exists()) {
+			$this->redirect('base/create_admin');
+		}
+
+	}
+
+	/**
+	* 生成auth权限表
+	*/
+	final public function create_auth()
+	{
+		if($this->auth_exists()) {
+			$this->redirect('index/index');
+		}
+	}
+
+	/**
+	* 判断是否已存在权限表
+	*/
+	final protected function auth_exists()
+	{
+		try {
+			Db::name('admin_user')->select();
+		} catch (\think\Exception $e) {
+			return false;
+		}
+		return true;
+
+	}
+
+
+	//创建admin用户
+	final public function create_admin()
+	{
+		if($this->auth_exists()) {
+			$this->redirect('index/index');
+		}
+		return $this->fetch();
+	}
+
+	//error_page
+	public function error_page()
+	{
+		$this->assign('error_msg', $this->error_msg);
+		return $this->fetch();
+	}
 
 }
--- app/common/controller/CBase.php
+++ app/common/controller/CBase.php
@@ -38,4 +38,5 @@ class CBase extends Controller
 
 	}
 
+
 }
