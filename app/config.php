04540f3c8a105ad1f4f968f9620088812941cc32
hiRainn <wr_sorarara@yahoo.co.jp>
2017-11-19 00:43:31 +0800

    验证码

--- app/config.php
+++ app/config.php
@@ -144,6 +144,24 @@ return [
         'view_base'    => __DIR__.'/../template/'
     ],
 
+    //验证码
+    'captcha'  => [
+        // 验证码字符集合
+        'codeSet'  => '12345678abcdefhijkmnpqrstuvwxyzABCDEFGHJKLMNPQRTUVWXY', 
+        // 验证码字体大小(px)
+        'fontSize' => 16, 
+        // 是否画混淆曲线
+        'useCurve' => false, 
+         // 验证码图片高度
+        'imageH'   => 30,
+        // 验证码图片宽度
+        'imageW'   => 100, 
+        // 验证码位数
+        'length'   => 4, 
+        // 验证成功后是否重置        
+        'reset'    => true
+    ],
+
     // 视图输出字符串内容替换
     'view_replace_str'       => [
         '__CSS__'   => '/static/css',
