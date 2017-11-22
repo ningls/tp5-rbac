56d922d7075b1ed9da954ca5c1dcd71695320806
hiRainn <wr_sorarara@yahoo.co.jp>
2017-11-22 20:47:01 +0800

    rbac

--- app/common/controller/CBase.php
+++ app/common/controller/CBase.php
@@ -6,38 +6,7 @@ use \app\common\exception\ErrorException;
 
 class CBase extends Controller
 {
-	// 错误类型 1-中止程序，2-选择是否继续
-	private $error_type; 
-
-	private $error_msg;
-
-	public function __construct()
-	{
-		parent::__construct();
-		try{
-			Db::Query("show databases");
-			Db::Query('show tables');
-		}
-		catch(\PDOException $e) {
-			$this->error_type = 1;
-			$this->error_msg = '当前数据库未配置';
-			$this->redirect('conf');
-		}
-		catch(\think\Exception $e) {
-			$this->error_type = 1;
-			$this->error_msg = 'database不存在';
-			$this->redirect('conf');
-		}
 		
-		$prefix = config('database.prefix');
-		if ($prefix === '') {
-			$this->error_type = 2;
-			$this->error_msg = '当前数据表前缀为空，是否继续？';
-			$this->redirect('conf');
-		}
-
-
-	}
 	/**
 	* 验证码
 	*/
