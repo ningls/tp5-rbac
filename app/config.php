a0735b9f1a858b0319b49048612d05a0ac3c4fa4
wangrun <wangrun001@microking.cn>
2017-11-24 16:36:42 +0800

    init back

--- app/config.php
+++ app/config.php
@@ -169,7 +169,8 @@ return [
         '__IMG__'    => '/static/img',
         '__ASSETS__' => '/static/assets',
         '__FONTS__'  => '/static/fonts',
-        '__LAYUI__'    => '/static/layui'
+        '__LAYUI__'  => '/static/layui',
+        '__LAYJS__'  => '/static/layer'
     ],
     // 默认跳转页面对应的模板文件
     'dispatch_success_tmpl'  => THINK_PATH . 'tpl' . DS . 'dispatch_jump.tpl',
