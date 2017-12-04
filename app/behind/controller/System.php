b24ad8266d0e4768475b51860efebc1a19195ac8
hiRainn <wr_sorarara@yahoo.co.jp>
2017-12-04 15:21:09 +0800

    行为日志

--- app/behind/controller/System.php
+++ app/behind/controller/System.php
@@ -1,5 +1,9 @@
 <?php
 namespace app\behind\controller;
+use \think\Request;
+use \app\behind\model\Adminlog;
+use \app\behind\model\AdminUser;
+use \app\common\logic\StatusCode;
 
 class System extends Base
 {
@@ -14,8 +18,40 @@ class System extends Base
     /**
      * 行为日志
      */
-    public function log()
+    public function log(Request $request):string
     {
-        dump(1);
+        $model = new adminLog();
+
+        $start = $request->get('start_time','');
+        $end = $request->get('end_time','');
+        $admin_id = $request->get('admin_id',0,'intval');
+        $where = [];
+        if($start !== '') {
+            $where['view_at'] = ['egt',strtotime($start)];
+        }
+        if($end !== '') {
+            $where['view_at'] = ['elt',strtotime($end) + 86400];
+        }
+        if($admin_id !== 0) {
+            $where['admin_id'] = $admin_id;
+        }
+        $page = $request->get('number_page',0,'intval')??$this->global_setting['page_limit'];
+        $data = $model->get_log($page, $where);
+
+        $user_model = new AdminUser();
+        $user_data = $user_model->get_user_id_and_name();
+        foreach($user_data as $k => $v) {
+            if($v['status'] != 0) {
+                $user_data[$k]['admin_name'] .= StatusCode::admin_user_status[$v['status']];
+            }
+        }
+        
+        $this->assign([
+            'user_data' => $user_data,
+            'log_data' => $data,
+            'page' => $data->render()
+        ]);
+        return $this->fetch();
+
     }
 }
--- app/behind/model/AdminLog.php
+++ app/behind/model/AdminLog.php
@@ -1,13 +1,24 @@
 <?php
 namespace app\behind\model;
+use \think\Model;
 
-class Adminlog extends Model
+class AdminLog extends Model
 {
 	/**
 	* 获取日志信息
 	*/
-	public function get_log($where,$limit)
+	public function get_log($limit, $where = [])
 	{
-		
+		$field = 'l.*,u.admin_name';
+		if($where !== []) {
+			return $this->alias('l')->field($field)->join('admin_user u','l.admin_id = u.id')->where($where)->order('l.id desc')->paginate($limit,false,[
+				'query'=>request()->param()
+			]);
+		} 
+		else {
+			return $this->alias('l')->field($field)->join('admin_user u','l.admin_id = u.id')->order('l.id desc')->paginate($limit,false,[
+				'query'=>request()->param()
+			]);
+		}
 	}
 }
